c41855d96cb3a1f2b6d5ff42937fdf02e50d7ffb
Ruslan Gorelov <lighttemper@gmail.com>
2017-09-14 00:40:01 +0700

    закончил 10 лабу

---
+++ z10-2.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Листинг 10.2. Вывод структуры и содержимого таблицы</title>
+        <meta charset="UTF-8">
+    <?php
+        include("z10-3.inc");
+        include("z10-4.inc");
+        include("z10-5.inc");
+        include("z10-6.inc");
+    ?>
+    </body>
+</html>
