f023229cac0c33955044a1847b8af138b97f143e
Ruslan Gorelov <lighttemper@gmail.com>
2017-12-06 09:30:15 +0700

    доработка построения регулярного выражения

--- script.js
+++ script.js
@@ -16,8 +16,24 @@ let parseDataFromInput = function() {
         "message": ""
     };
 
+    let items = pick.split('');
+    for (let i = 0; i < items.length; i++) {
+        if (alph.indexOf(items[i]) == -1) {
+            errors.message = "Подцепочка '" + pick + "' содержит символы, не входящие в состав алфавита";
+          
+            return errors;
+        } 
+    }
+
     if (!alph.length && !mult && !pick) {
         errors.message = "Заполните поля начальными данными";
+
+        return errors;
+    }
+
+    if (isNaN(+mult)) {
+        errors.element = "Кратность";
+        errors.message = "В поле " + errors.element + " введеные не числовые значения.";
 
         return errors;
     }
@@ -34,22 +50,6 @@ let parseDataFromInput = function() {
         return errors;
     }
 
-    if (pick.length == 0) {
-        errors.element = "Подцепочка";
-        errors.message = "Поле " + errors.element + " не может быть пустым.";
-
-        return errors;
-    } else {
-        pick.split('').forEach(item => {
-            if (alph.indexOf(item) == -1) {
-                errors.element = "Подцепочка";
-                errors.message = "Подцепочка " + pick + " содержит символы, не входящие в состав алфавита";
-                
-                return errors;
-            } 
-        });
-    }
-
     if (!alph.length) {
         errors.element = "Алфавит";
         errors.message = "Алфавит не может быть пустым";
@@ -63,6 +63,7 @@ let parseDataFromInput = function() {
 
 let generationRegExp = function() {
     let obj = parseDataFromInput();
+    console.log(obj);
     if (obj.check == undefined) {
         let exp = "";
         if (+obj.mult == obj.pick.length) {
@@ -87,29 +88,43 @@ let generationRegExp = function() {
 
             exp += "(";
             //главная часть выражения, где перебираются все варианты
-            for(let i = 0; i < lengthOfRest + 1; i++) {
-                for (let j = 0; j < lengthOfRest + 1; j++) {
-                    if (j == i) { 
-                        exp += obj.pick;
-                    } else {
-                        exp += anySymbol;
+            if (obj.pick.length != 0) {
+                for(let i = 0; i < obj.mult; i++) {
+                    let check = true;
+                    for (let j = 0; j < obj.mult; j++) {
+                        if (j == i && check) { 
+                            exp += obj.pick;
+                            j += obj.pick.length;
+
+                            if (j > obj.mult) {
+                                j -= obj.mult;
+                                check = false;  
+                            }
+                            j--;
+                        } else {
+                            exp += anySymbol;
+                        }
+                    }
+
+                    if (i < obj.mult - 1) {
+                        exp += "+";
                     }
                 }
-
-                if (i < lengthOfRest) {
-                    exp += "+";
-                }
-            }
-            exp += ")";
+                exp += ")";
+            } 
 
             //граничная часть выражения
             let boundariesOfExp = "(";
             for (let i = 0; i < +obj.mult; i++) {
                 boundariesOfExp += anySymbol; 
             }
-            boundariesOfExp += ")*"
+            boundariesOfExp += ")*";
             console.log(boundariesOfExp);
-            exp = boundariesOfExp + exp + boundariesOfExp;
+            if (obj.pick.length == 0) {
+                exp = boundariesOfExp;
+            } else {
+                exp = boundariesOfExp + exp + boundariesOfExp;
+            }
         }
 
         return exp;
@@ -122,6 +137,7 @@ window.onload = function() {
     const genBtn = document.getElementsByClassName('btn-gen')[0];
     const tskBtn = document.getElementsByClassName('choice-btn task')[0];
     const closeBtn = document.getElementsByClassName('close-win')[0];
+    //const multInp = document.getElementsByClassName('input-multiplicity')[0];
     //console.log(genBtn);
     tskBtn.onclick = function() {
         document.getElementById('task-form').style.display = "";
@@ -132,7 +148,7 @@ window.onload = function() {
         document.getElementById('task-form').style.display = "none";
         document.getElementById('main-form').style.display = "";
     };
-
+    
     genBtn.onclick = function(e) {
         document.getElementById('area-exp').innerHTML = generationRegExp(); 
         //console.log(generationRegExp());
