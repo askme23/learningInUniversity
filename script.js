a6c5e86cf0be28036ec0e80de15453d4a27799f1
Gorelov Ruslan <lighttemper@gmail.com>
2017-12-12 14:58:18 +0700

    добавлена логика построения цепочек

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let parseDataFromInput = function() {
     let alph = document.getElementsByClassName('input-alphabet')[0].value;
     const mult = document.getElementsByClassName('input-multiplicity')[0].value;
     const pick = document.getElementsByClassName('input-pickAp')[0].value;
-    console.log(alph);
+    //console.log(alph);
     alph = alph.replace(/[,\.\|\\\s\-]/g, '').split('');
 
     const objWithStartData = {
@@ -34,7 +34,7 @@ let parseDataFromInput = function() {
 
     if (isNaN(+mult)) {
         errors.element = "Кратность";
-        errors.message = "В поле " + errors.element + " введеные не числовые значения.";
+        errors.message = "В поле " + errors.element + " введенные не числовые значения.";
 
         return errors;
     }
@@ -89,7 +89,7 @@ let generationRegExp = function() {
         if (obj.pick.length != 0) {
             for(let i = 0; i < obj.mult; i++) {
                 let check = true;
-                for (let j = 0; j < obj.mult; j++) {
+                for (let j = 0; j < obj.mult; j++) { 
                     if (j == i && check) { 
                         exp += obj.pick;
                         j += obj.pick.length;
@@ -123,7 +123,6 @@ let generationRegExp = function() {
         } else {
             exp = boundariesOfExp + exp + boundariesOfExp;
         }
-        
 
         return exp;
     } else {
@@ -131,8 +130,97 @@ let generationRegExp = function() {
     }
 };
 
+//функция для генерации цепочек
+let generationChains = function(reg, range) {
+    var res = [];
+    var cur = [];
+
+    cur.push("");
+    for (var i = 0; i < reg.length; ++i) {
+        if (reg[i] == '(') {
+            var d = 0;//счетчик
+            var next = "";
+
+            while (i < reg.length) {
+                next += reg[i];
+                if (reg[i] == '(') {
+                    ++d;
+                } else if (reg[i] == ')') {
+                    --d;
+                }
+
+                if (d == 0) {
+                    break;
+                }
+
+                ++i;
+            }
+
+            var rep = false;
+            if (i + 1 < reg.length && reg[i+1] == '*') {
+                ++i;
+                rep = true;
+            }
+            //console.log(rep)
+
+            
+            var inc = generationChains(next.substring(1, next.length - 1), range);
+            var newcur = [];
+            if (rep) {
+                var newinc = [];
+                newinc.push("");
+                var add = true;
+
+                while (add) {
+                    add = false;
+                    for (var j = 0; j < inc.length; ++j) {
+                        for (var k = 0; k < newinc.length; ++k) {
+                            var newstr = newinc[k] + inc[j];
+
+                            if (newstr.length <= range && newinc.indexOf(newstr) == -1) {
+                                newinc.push(newstr);
+                                add = true;
+                            }
+                        }
+                    }
+                }
+                inc = newinc;
+
+            }
+
+            for (var j = 0; j < inc.length; ++j) {
+                for (var k = 0; k < cur.length; ++k) {
+                    var newstr = cur[k] + inc[j];
+                    if (newstr.length <= range) {
+                        newcur.push(newstr);
+                    }
+                }
+            }
+
+            cur = newcur;
+        } else if (reg[i] == '+') {
+            cur.forEach((item) => {
+                res.push(item);
+            });
+            //console.log(res);
+            cur = [];
+            cur.push("");
+        } else {
+            for (var j = 0; j < cur.length; ++j)
+                cur[j] += reg[i];
+        }
+    }
+    cur.forEach((item) => {
+        res.push(item);
+    });
+    cur = [];
+
+    return res;
+};
+
 window.onload = function() {
-    const genBtn = document.getElementsByClassName('btn-gen')[0];
+    const genBtnReg = document.getElementsByClassName('btn-gen reg')[0];
+    const genBtnChains = document.getElementsByClassName('btn-gen chains')[0];
     const tskBtn = document.getElementsByClassName('choice-btn task')[0];
     const closeBtn = document.getElementsByClassName('close-win')[0];
     //const multInp = document.getElementsByClassName('input-multiplicity')[0];
@@ -147,10 +235,42 @@ window.onload = function() {
         document.getElementById('main-form').style.display = "";
     };
 
-    genBtn.onclick = function(e) {
-        document.getElementById('area-exp').innerHTML = generationRegExp(); 
+    genBtnReg.onclick = function(event) {
         //console.log(generationRegExp());
+        if (generationRegExp()) {
+            document.getElementById('area-exp').innerHTML = generationRegExp(); 
+        } else {
+            document.getElementById('area-exp').innerHTML = ""; 
+        }
+        // console.log(document.getElementById('area-exp'));
     };
-    
 
+    genBtnChains.onclick = function() {
+        const areaExp = document.getElementById('area-exp');
+        const areaConv = document.getElementById('area-conv');
+        const range = [+document.getElementsByClassName('range')[0].value, +document.getElementsByClassName('range')[1].value];
+        let obj = parseDataFromInput();
+
+        console.log(areaExp.innerHTML + ' - ' + range[1]);
+        //вывод цепочек в указанном диапазоне и указанных условиях вывода, а также проверка на валидность данных
+        if (obj.check == undefined) {
+            if (areaExp.value.length != 0) {
+                const chains = generationChains(areaExp.value, range[1]).sort((a, b) => {
+                    return a.length - b.length;
+                });
+
+                console.log(chains);
+                for(let i = 0; i < chains.length; i++) {
+                    if (chains[i].length >= range[0] && 
+                        chains[i].length <= range[1] && 
+                        chains[i].length % obj.mult == 0 && 
+                        chains[i].indexOf(obj.pick) != -1) {
+                        areaConv.value += chains[i] + '\n';
+                    }
+                }
+            }
+        } else {
+            alert(obj.message);
+        }
+    };
 };
